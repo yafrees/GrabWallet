2a8f09eec8c50cd9e8db15ce45e9d9ce23754958
yafrees <yafree18@gmail.com>
2016-05-24 16:02:19 +0800

    主代码。

--- app/src/main/java/com/example/yf/grabwallet/GrabWallet.java
+++ app/src/main/java/com/example/yf/grabwallet/GrabWallet.java
@@ -1,29 +1,105 @@
 package com.example.yf.grabwallet;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.accessibilityservice.AccessibilityService;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
 
-public class GrabWallet extends AppCompatActivity {
+import java.util.List;
+
+public class GrabWallet extends AccessibilityService {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_grab_wallet);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    public void onAccessibilityEvent(AccessibilityEvent event) {
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        int eventType = event.getEventType();
+        switch (eventType){
+            //第一步监听通知栏消息
+            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
+                List<CharSequence> texts = event.getText();
+                if (!texts.isEmpty()){
+                    for (CharSequence text : texts){
+                        String content = text.toString();
+                        if (content.contains("[微信红包]")){
+                            //模拟打开通知栏消息
+                            if (event.getParcelableData() != null && event.getParcelableData() instanceof Notification){
+                                Notification notification = (Notification) event.getParcelableData();
+                                PendingIntent pendingIntent = notification.contentIntent;
+                                try {
+                                    pendingIntent.send();
+                                }
+                                catch (PendingIntent.CanceledException e){
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                }
+                break;
+            //第二部：监听是都进入微信红包消息界面
+            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
+                String className = event.getClassName().toString();
+                if (className.equals("com.tencent.mm.ui.LauncherUI")){
+                    //开始抢红包
+                    getPacket();
+                }
+                else if (className.equals("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI")){
+                    //开始打开红包
+                    openPacket();
+                }
+                break;
+        }
+
     }
 
+    //打开红包
+    private void openPacket() {
+        AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
+        if (nodeInfo != null){
+            List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByText("抢红包");
+            for (AccessibilityNodeInfo n : list){
+                n.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            }
+        }
+
+    }
+
+    //抢红包
+    private void getPacket() {
+        AccessibilityNodeInfo rootNode = getRootInActiveWindow();
+        recycle(rootNode);
+    }
+
+    private void recycle(AccessibilityNodeInfo info) {
+        if (info.getChildCount() == 0){
+            if (info.getText() != null){
+                if ("開".equals(info.getText().toString())){
+                    info.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                    AccessibilityNodeInfo parent = info.getParent();
+                    while (parent != null){
+                        if (parent.isClickable()){
+                            parent.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                            break;
+                        }
+                        parent = parent.getParent();
+                    }
+                }
+            }
+        }
+        else {
+            for (int i = 0 ; i < info.getChildCount() ; i++){
+                if (info.getChild(i) != null){
+                    recycle(info.getChild(i));
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
 }
